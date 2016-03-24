76108ea4bd2a095962aa51a6e66531d331ac1cab
stefandevai <stedevai@gmail.com>
2016-03-23 22:35:06 -0600

    Starting to refactor renderables classes

--- src/graphics/renderable.cpp
+++ src/graphics/renderable.cpp
@@ -1,5 +1,25 @@
 #include "renderable.h"
 
 namespace stella { namespace graphics {
+  Renderable::Renderable(GLint x, GLint y, GLint w, GLint h, Texture &texture, GLboolean visible)
+    : Tex(texture)
+  {
+    this->Pos = glm::vec2((GLfloat)x, (GLfloat)y);
+    this->Dimensions = glm::vec2((GLfloat)w, (GLfloat)h);
+    this->Visible = visible;
+  }
+  
+  Renderable::Renderable(Texture &texture)
+    : Tex(texture)
+  {
+    this->Pos = glm::vec2(0.0f, 0.0f);
+    this->Dimensions = glm::vec2(this->Tex.GetWidth(), this->Tex.GetHeight());
+    this->Visible = GL_TRUE;
+  }
+
+  Renderable::~Renderable()
+  {
+    
+  }
 } }
 
--- src/graphics/renderable.h
+++ src/graphics/renderable.h
@@ -1,9 +1,33 @@
 #pragma once
 
+#include <vector>
+#include "texture.h"
+#include <GL/glew.h>
+#include "../../Dependencies/glm/glm/glm.hpp"
+
 namespace stella { namespace graphics {
   class Renderable
   {
+    public:
+      glm::vec2 Pos, Dimensions;
 
+      Renderable(GLint x, GLint y, GLint w, GLint h, Texture &texture, GLboolean visible = GL_TRUE);
+      Renderable(Texture &texture);
+      virtual ~Renderable();
+
+      inline const glm::vec2 GetPos() const { return Pos; }
+      inline const glm::vec2 GetDimensions() const { return Dimensions; }
+      inline const GLuint GetTexID() const { return Tex.GetID(); }
+      inline const std::string& GetTexName() const { return Tex.GetName(); }
+      inline Texture* GetTexture() const { return &Tex; }
+      inline const GLboolean IsVisible() const { return Visible; }
+
+      void SetVisible(GLboolean vis) { Visible = vis; }
+      void ToggleVisible() { Visible = !Visible; }
+
+    protected:
+      Texture &Tex;
+      GLboolean Visible;
   };
 } }
 
